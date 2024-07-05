3d11c27ccf57715a7aade4b6d78cea172c0ae9bd
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-07-05 14:20:16 +0100

    finish ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,54 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let payload: any;
+  let clientIP: any;
+  let geoData: any;
+
+  let continent: any;
+  let country: string;
+  let city: string;
+  let region: string;
+  let localTime: any;
+
+  let options: RequestInit = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  onMount(async () => {
+    const dataResponse = await fetch(
+      `https://where-am-i-api.vercel.app/api/get-me`,
+      options
+    );
+
+    payload = await dataResponse.json();
+
+    console.log(payload);
+
+    clientIP = payload["client_ip"];
+    geoData = payload["geodata"];
+
+    continent = geoData["continent"];
+    country = geoData["country"];
+    city = geoData["city"];
+    region = geoData["region"];
+    localTime = geoData["localtime"];
+  });
+</script>
+
 <div class="container">
   <p class="title">i know where you are!</p>
+  <p class="subtitle">your IP address: {clientIP}</p>
   <div class="data">
-    <p>continent: europe</p>
-    <p>country: Spain</p>
+    <p>continent: {continent}</p>
+    <p>country: {country}</p>
 
-    <p>city: Madrid</p>
-    <p>local time: 20:13</p>
+    <p>region: {region}</p>
+    <p>city: {city}</p>
+    <p>local time: {localTime}</p>
   </div>
 </div>
 
@@ -21,6 +64,14 @@
     font-size: 3rem;
     text-align: center;
     text-transform: capitalize;
+    margin: 0;
+  }
+
+  .subtitle {
+    font-weight: 700;
+    font-size: 2rem;
+    text-align: center;
+    text-transform: capitalize;
   }
 
   .data {
@@ -30,6 +81,7 @@
   }
 
   .data > p {
+    text-align: center;
     font-size: 1.5rem;
     text-transform: capitalize;
     margin: 0;
