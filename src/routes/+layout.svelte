e43da24068c82057f81cf37dd199b0493be65dde
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-07-05 12:34:30 +0100

    design

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import "../app.css";
+</script>
+
+<svelte:head>
+  <title>I know where you are</title>
+</svelte:head>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+  <p class="title">i know where you are!</p>
+  <div class="data">
+    <p>continent: europe</p>
+    <p>country: Spain</p>
+
+    <p>city: Madrid</p>
+    <p>local time: 20:13</p>
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .title {
+    font-weight: 700;
+    font-size: 3rem;
+    text-align: center;
+    text-transform: capitalize;
+  }
+
+  .data {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .data > p {
+    font-size: 1.5rem;
+    text-transform: capitalize;
+    margin: 0;
+  }
+</style>
